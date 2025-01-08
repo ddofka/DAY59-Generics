f1c4a1be0e1d5baefcee770bd98c6ebbab86ad25
ddofka <ddorofejevas@yahoo.com>
2025-01-08 21:56:37 +0200

    uzduotis nr 2.2, papildomi metodai

--- src/Main.java
+++ src/Main.java
@@ -17,11 +17,37 @@ public class Main {
         printList(skaiciai);
         printList(eilutes);
 
-        System.out.println();
+        System.out.println("-".repeat(50));
 
         Azuolas azuolas = new Azuolas();
         azuolas.turi();
 
+        System.out.println("-".repeat(50));
+
+        Berzas berzas = new Berzas();
+        Berzas berzas2 = new Berzas();
+        Berzas berzas3 = new Berzas();
+        Egle egle = new Egle();
+        Kadagys kadagys = new Kadagys();
+        Pusis pusis = new Pusis();
+
+        List<Medis> medziai = new ArrayList<>();
+        medziai.add(azuolas);
+        medziai.add(berzas);
+        medziai.add(kadagys);
+        medziai.add(pusis);
+        List<Spygliuotis> spygliuociai = new ArrayList<>();
+        spygliuociai.add(egle);
+        spygliuociai.add(pusis);
+        List<Berzas> berzai = new ArrayList<>();
+        berzai.add(berzas);
+        berzai.add(berzas2);
+        berzai.add(berzas3);
+
+
+        medziuMetodai.ivairusMiskas(medziai);
+        medziuMetodai.spygliuociuMiskas(spygliuociai);
+        medziuMetodai.berzuMiskas(berzai);
     }
 
     public static void printList(List<?> list){
---
+++ src/medziuMetodai.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class medziuMetodai {
+
+    public static void ivairusMiskas(List<? extends Medis> list){
+        System.out.println("---Įvairus miškas---");
+        for (Medis m:list){
+            m.turi();
+        }
+    }
+
+    public static void spygliuociuMiskas(List<? extends Spygliuotis> list){
+        System.out.println("---Spygliuočių miškas---");
+        for (Spygliuotis s:list){
+            s.turi();
+        }
+    }
+
+    public static void berzuMiskas(List<? extends Berzas> list){
+        System.out.println("---Beržų miškas---");
+        for (Berzas b:list){
+            b.turi();
+        }
+    }
+
+}
